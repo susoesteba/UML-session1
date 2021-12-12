3ad4c660a1b8b39e0842a301abf5836a26833409
Cédric Esteba Serra <cedricbri@gmail.com>
2021-12-12 18:54:55 +0100

    Add elements

---
+++ uml.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn import datasets
+from kmeans_class import Kmeans
+
+X = datasets.load_iris().data
+np.random.seed(1)
+K = 3
+km = Kmeans(X, K)
+
+S0 = X[(0,1,2),:]
+
+km = km.set_centers(S0)
+km = km.assignment_step()
+km.plot("1st assignment")
+km = km.updating_step().assignment_step()
+km.plot("2nd assignment")
